e4397513387f5261ff91d9ceafe938dcc87ea189
juanicarre <juanicarrenio@gmail.com>
2022-11-16 12:50:14 -0300

    anteultima pr

--- dashboard/src/components/products/AddProductMain.js
+++ dashboard/src/components/products/AddProductMain.js
@@ -125,7 +125,7 @@ const AddProductMain = () => {
                     <select
                       onChange={(e) => handleSelect(e.target.value)} class="block form-control appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
                       <option disabled selected defaultValue>
-                        Category
+                        Categories
                       </option>
                       {
                           categories.map(e =>
@@ -133,7 +133,7 @@ const AddProductMain = () => {
                         )
                       }
                     </select>
-                    <h3>{categorias.join(" ")}.</h3>
+                    <h3>{categorias.length?categorias.join(", "): "Choose at least one category."}</h3>
                   </div>
                   <div className="mb-4">
                     <label className="form-label">Description</label>
